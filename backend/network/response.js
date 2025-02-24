65ad285160a061634c8641f73ffeb0a58a71c4ee
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2025-02-23 21:19:52 -0500

    fix

--- backend/network/response.js
+++
@@ -1,7 +0,0 @@
-exports.success = function(req, res, data, num_status) {
-    res.status( num_status ).send( { error:'', body:data })
-}
-
-exports.error = function(req, res, message, num_status) {
-    res.status( num_status ).send( { error:message, body:'' } )
-}
